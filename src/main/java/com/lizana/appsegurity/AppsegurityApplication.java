f292e2a2b3f6e975ee1fab91e97705530c220f24
daniel <daniel.lizana01@gmail.com>
2024-06-17 01:15:27 -0500

    first commit

---
+++ src/main/java/com/lizana/appsegurity/AppsegurityApplication.java
@@ -0,0 +1,13 @@
+package com.lizana.appsegurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsegurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsegurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lizana/appsegurity/AppsegurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lizana.appsegurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppsegurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
