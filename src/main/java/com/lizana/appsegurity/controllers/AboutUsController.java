fcea085e12443cd968d08b7e2b66b16c8c149896
daniel <daniel.lizana01@gmail.com>
2024-06-17 21:57:59 -0500

    basico

---
+++ src/main/java/com/lizana/appsegurity/controllers/AboutUsController.java
@@ -0,0 +1,19 @@
+package com.lizana.appsegurity.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@RequestMapping(path = "/about_us")
+public class AboutUsController {
+
+    @GetMapping
+    public Map<String,String> welcome(){
+        return Collections.singletonMap("msj","abouts");
+    }
+}
---
+++ src/main/java/com/lizana/appsegurity/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.lizana.appsegurity.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    /*
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }*/
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/loans","/balance","accounts","/cards").authenticated()
+                                .anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+
+}
