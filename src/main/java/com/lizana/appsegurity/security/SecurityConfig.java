ef32ebf808bb43ede0a381c60f05d975b5e4d72d
daniel <daniel.lizana01@gmail.com>
2024-06-18 22:55:06 -0500

    se configuro con spring y base de datos asegurado

--- src/main/java/com/lizana/appsegurity/security/SecurityConfig.java
+++ src/main/java/com/lizana/appsegurity/security/SecurityConfig.java
@@ -1,11 +1,22 @@
 package com.lizana.appsegurity.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
@@ -23,12 +34,31 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/loans","/balance","accounts","/cards").authenticated()
+                        auth.requestMatchers("/loans", "/balance", "accounts", "/cards").authenticated()
                                 .anyRequest().permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    /*@Bean
+    InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+
+        UserDetails admin = User.withUsername("admin").password("to_be_encoded").authorities("ADMIN").build();
+        UserDetails user = User.withUsername("user").password("to_be_encoded").authorities("USER").build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }*/
+
+    @Bean
+    UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 
 
